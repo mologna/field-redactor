87d0ce56cdc5bb87831f890249a67cf05a6ea0f6
mologna <molony.andrew@gmail.com>
2025-01-07 13:34:20 -0700

    Add basic webpack for bundling

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: [
+          /node_modules/,
+          /tests/
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
