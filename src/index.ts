60f0ea5239309463d050d857ae18b25ca772b6f2
mologna <molony.andrew@gmail.com>
2025-01-07 15:44:25 -0700

    Switch to tsc from webpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { FieldRedactor } from './fieldRedactor';
 export { CustomObjectMatchType, Redactor, FieldRedactorConfig, CustomObject } from './types';
 export { FieldRedactor };
-export default FieldRedactor;
